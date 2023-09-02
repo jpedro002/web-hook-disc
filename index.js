841143ef202a2937c43f210608d8053a19a8cf17
jpedro002 <joaopsilvavolei@gmail.com>
2023-09-02 11:27:25 -0300

    chore:up first file

---
+++ index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+const pathToWrite = path.join(__dirname , 'index.html')
+
+const data = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Olá Node</h1>
+</body>
+</html>
+`
+console.log(pathToWrite)
+fs.writeFileSync(pathToWrite, data);
